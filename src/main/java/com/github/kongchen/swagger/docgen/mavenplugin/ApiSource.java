7038ab4e2ecf7a69e2fb759c8550525063850410
Tomasz Juchniewicz <tjuchniewicz@gmail.com>
2018-07-17 20:36:50 +0200

    Introduce skipInheritingClasses parameter
    Fix codeclimate issue.
    
    Fixes gh-635.

--- src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/ApiSource.java
+++ src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/ApiSource.java
@@ -140,26 +140,23 @@ public class ApiSource {
 
     public Set<Class<?>> getValidClasses(Class<? extends Annotation> clazz) {
         Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
+        
+        List<String> prefixes = new ArrayList<String>();
         if (getLocations() == null) {
-            Set<Class<?>> c = new Reflections("").getTypesAnnotatedWith(clazz, true);
+            prefixes.add("");
+        } else {
+            prefixes.addAll(getLocations());
+        }
+        
+        for (String location : prefixes) {
+            Set<Class<?>> c = new Reflections(location).getTypesAnnotatedWith(clazz, true);
             classes.addAll(c);
 
             if (!skipInheritingClasses) {
-                Set<Class<?>> inherited = new Reflections("").getTypesAnnotatedWith(clazz);
+                Set<Class<?>> inherited = new Reflections(location).getTypesAnnotatedWith(clazz);
                 classes.addAll(inherited);
             }
-        } else {
-            for (String location : locations) {
-                Set<Class<?>> c = new Reflections(location).getTypesAnnotatedWith(clazz, true);
-                classes.addAll(c);
-
-                if (!skipInheritingClasses) {
-                    Set<Class<?>> inherited = new Reflections(location).getTypesAnnotatedWith(clazz);
-                    classes.addAll(inherited);
-                }
-            }
         }
-
         return classes;
     }
     
